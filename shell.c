8a77ef43da11e05dea966db7982525118ec1c2c0
Isan Rivkin <isan.rivkin@similarweb.com>
2019-06-06 15:10:36 +0300

    initial commit

---
+++ shell.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <memory.h>
+
+char* build_ssh_command(char * terminal_cmd, char * user_name, char * ip){
+    char * ssh_cmd = (char *) malloc(4 + strlen(user_name)+ strlen(ip));
+    if(ssh_cmd == NULL){
+        return NULL;
+    }
+    strcpy(ssh_cmd,"ssh ");
+    strcat(ssh_cmd,user_name);
+    strcat(ssh_cmd,"@");
+    strcat(ssh_cmd,ip);
+    // append the ssh command with terminal openning 
+    char * ssh_with_terminal_cmd = (char *) malloc(2 + strlen(terminal_cmd)+ strlen(ssh_cmd));
+    if(ssh_with_terminal_cmd == NULL){
+        return NULL;
+    }
+    strcpy(ssh_with_terminal_cmd,terminal_cmd);
+    strcat(ssh_with_terminal_cmd,ssh_cmd);
+    strcat(ssh_with_terminal_cmd,"'");
+    return ssh_with_terminal_cmd;
+}
+int main(int argc, char *argv[]) {
+    // input must have a user name param and atleast 1 ip address 
+    if(argc < 3){
+        return -1; 
+    }
+    char* user_name = argv[1]; 
+    char * terminal_cmd = "gnome-terminal -x sh -c '";
+    for(int i=2;i<argc;i++){
+        // build the ssh command 
+        char * ip = argv[i];
+        //gnome-terminal -x sh -c 'ssh user_name@ip'
+        char * ssh_with_terminal_cmd = build_ssh_command(terminal_cmd,user_name,ip);
+        if(ssh_with_terminal_cmd == NULL){
+            return -2; 
+        }
+        printf("executing =|%s|",ssh_with_terminal_cmd);
+        // execute the command 
+        int exit_status = system(ssh_with_terminal_cmd);
+        if(exit_status != 0){
+            printf("\n ERROR %d executing command %s",exit_status,ssh_with_terminal_cmd);
+        }else{
+            printf("Ok! %s", ip);
+        }
+    }
+}
